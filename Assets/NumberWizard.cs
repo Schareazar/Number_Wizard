b4c74addeb8aa47c2eeed3700f1e69c797e4da1b
Schareazar <kukawski.marcin@gmail.com>
2020-03-16 22:14:35 +0100

    print+var

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,9 +7,11 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int maxGuess = 1111;
+        int minGuess = 111;
         Debug.Log("Hi, this is the number wizard");
         Debug.Log("Pick a number for me to guess");
-        Debug.Log("You can pick between 0 and 1000");
+        Debug.Log("You can pick a number between " + minGuess +  " and " + maxGuess);
     }
 
     // Update is called once per frame
