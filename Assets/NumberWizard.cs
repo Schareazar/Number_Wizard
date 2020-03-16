c1cf2736e9a8db92a64906f3148e01d2ddca4b7e
Schareazar <kukawski.marcin@gmail.com>
2020-03-16 21:36:12 +0100

    Initial commit

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hi, this is the number wizard");
+        Debug.Log("Pick a number for me to guess");
+        Debug.Log("You can pick between 0 and 1000");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
