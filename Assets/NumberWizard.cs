2bf1380322ba80e29d0387f7cbdef94376b076e1
Schareazar <kukawski.marcin@gmail.com>
2020-03-18 20:36:00 +0100

    refactor

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    int maxGuess = 1000;
-    int minGuess = 1;
-    int guess = 500;
+    int maxGuess;
+    int minGuess;
+    int guess;
 
     // Start is called before the first frame update
     void Start()
     {
+        StartGameMessage();
+    }
+
+    void StartGameMessage()
+    {
+        maxGuess = 1000;
+        minGuess = 1;
+        guess = 500;
+
         Debug.Log("Hi, this is the number wizard");
         Debug.Log("Pick a number for me to guess");
-        Debug.Log("You can pick a number between " + minGuess +  " and " + maxGuess);
+        Debug.Log("You can pick a number between " + minGuess + " and " + maxGuess);
         Debug.Log("Tell me if your number is higher, lower or equal to my guess");
         Debug.Log("If your number is higher press Arrow Up, \n If your number is lower press Arrow Down");
         Debug.Log("If my guess is correct press Enter");
@@ -27,19 +36,23 @@ public class NumberWizard : MonoBehaviour
         {
             Debug.Log("So the number you selected is higher");
             minGuess = guess;
-            guess = (minGuess + maxGuess +1) / 2;
-            Debug.Log("What about " + guess + "?");
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("So the number you selected is lower");
             maxGuess = guess;
-            guess = (minGuess + maxGuess) / 2;
-            Debug.Log("What about " + guess + "?");
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("So I guessed your number correctly! \n Your number was " + guess);
+            StartGameMessage();
         }
+    }
+    void NextGuess()
+    {
+        guess = (minGuess + maxGuess + 1) / 2;
+        Debug.Log("What about " + guess + "?");
     }
 }
